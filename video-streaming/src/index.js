7b33dba3b9a77a21b26a25c294682e65e2e4e5b6
Viktor <derfikus@gmail.com>
2026-03-24 21:21:47 +0200

    Fixed spacing in video-streaming app

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
-const amqp = require('amqplib');
+const amqp = require("amqplib");
 const axios = require("axios");
 
 if (!process.env.PORT) {
-    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+  throw new Error(
+    "Please specify the port number for the HTTP server with the environment variable PORT.",
+  );
 }
 
 if (!process.env.RABBIT) {
-    throw new Error("Please specify the name of the RabbitMQ host using environment variable RABBIT");
+  throw new Error(
+    "Please specify the name of the RabbitMQ host using environment variable RABBIT",
+  );
 }
 
 const PORT = process.env.PORT;
@@ -17,46 +21,48 @@ const RABBIT = process.env.RABBIT;
 // Application entry point.
 //
 async function main() {
-    const messagingConnection = await amqp.connect(RABBIT); // Connects to the RabbitMQ server.
-    
-    const messageChannel = await messagingConnection.createChannel(); // Creates a RabbitMQ messaging channel.
-    
-    await messageChannel.assertExchange("viewed", "fanout"); // Asserts that we have a "viewed" exchange.
+  const messagingConnection = await amqp.connect(RABBIT); // Connects to the RabbitMQ server.
 
-    //
-    // Broadcasts the "viewed" message to other microservices.
-    //
-    function broadcastViewedMessage(messageChannel, videoId) {
-        console.log(`Publishing message on "viewed" exchange.`);
-            
-        const msg = { video: { id: videoId } };
-        const jsonMsg = JSON.stringify(msg);
-        messageChannel.publish("viewed", "", Buffer.from(jsonMsg)); // Publishes message to the "viewed" exchange.
-    }
+  const messageChannel = await messagingConnection.createChannel(); // Creates a RabbitMQ messaging channel.
 
-    const app = express();
+  await messageChannel.assertExchange("viewed", "fanout"); // Asserts that we have a "viewed" exchange.
 
-    app.get("/video", async (req, res) => { // Route for streaming video.
+  //
+  // Broadcasts the "viewed" message to other microservices.
+  //
+  function broadcastViewedMessage(messageChannel, videoId) {
+    console.log(`Publishing message on "viewed" exchange.`);
 
-        const videoId = req.query.id;
-        const response = await axios({ // Forwards the request to the video-storage microservice.
-            method: "GET",
-            url: `http://video-storage/video?id=${videoId}`, 
-            data: req, 
-            responseType: "stream",
-        });
-        response.data.pipe(res);
+    const msg = { video: { id: videoId } };
+    const jsonMsg = JSON.stringify(msg);
+    messageChannel.publish("viewed", "", Buffer.from(jsonMsg)); // Publishes message to the "viewed" exchange.
+  }
 
-        broadcastViewedMessage(messageChannel, videoId); // Sends the "viewed" message to indicate this video has been watched.
+  const app = express();
+
+  app.get("/video", async (req, res) => {
+    // Route for streaming video.
+
+    const videoId = req.query.id;
+    const response = await axios({
+      // Forwards the request to the video-storage microservice.
+      method: "GET",
+      url: `http://video-storage/video?id=${videoId}`,
+      data: req,
+      responseType: "stream",
     });
+    response.data.pipe(res);
 
-    app.listen(PORT, () => { // Starts the HTTP server.
-        console.log("Microservice online.");
-    });
+    broadcastViewedMessage(messageChannel, videoId); // Sends the "viewed" message to indicate this video has been watched.
+  });
+
+  app.listen(PORT, () => {
+    // Starts the HTTP server.
+    console.log("Microservice is online online.");
+  });
 }
 
-main()
-    .catch(err => {
-        console.error("Microservice failed to start.");
-        console.error(err && err.stack || err);
-    });
+main().catch((err) => {
+  console.error("Microservice failed to start.");
+  console.error((err && err.stack) || err);
+});
