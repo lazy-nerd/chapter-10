4939af76b3b2a7b5748587f93e1a759841168978
Viktor <derfikus@gmail.com>
2026-03-24 21:21:01 +0200

    Fix mock-storage log

--- mock-storage/src/index.js
+++ mock-storage/src/index.js
@@ -3,7 +3,9 @@ const fs = require("fs");
 const path = require("path");
 
 if (!process.env.PORT) {
-    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+  throw new Error(
+    "Please specify the port number for the HTTP server with the environment variable PORT.",
+  );
 }
 
 const PORT = process.env.PORT;
@@ -17,30 +19,29 @@ const app = express();
 // HTTP GET route that streams a video from storage.
 //
 app.get("/video", (req, res) => {
-
-    const videoId = req.query.id;
-    const localFilePath = path.join(storagePath, videoId);
-    res.sendFile(localFilePath);
+  const videoId = req.query.id;
+  const localFilePath = path.join(storagePath, videoId);
+  res.sendFile(localFilePath);
 });
 
 //
 // HTTP POST route to upload a video to storage.
 //
 app.post("/upload", (req, res) => {
-
-    const videoId = req.headers.id;
-    const localFilePath = path.join(storagePath, videoId);
-    const fileWriteStream = fs.createWriteStream(localFilePath);
-    req.pipe(fileWriteStream)
-        .on("error", err => {
-            console.error("Upload failed.");
-            console.error(err && err.stack || err);
-        })
-        .on("finish", () => {
-            res.sendStatus(200);
-        });
+  const videoId = req.headers.id;
+  const localFilePath = path.join(storagePath, videoId);
+  const fileWriteStream = fs.createWriteStream(localFilePath);
+  req
+    .pipe(fileWriteStream)
+    .on("error", (err) => {
+      console.error("Upload failed.");
+      console.error((err && err.stack) || err);
+    })
+    .on("finish", () => {
+      res.sendStatus(200);
+    });
 });
 
 app.listen(PORT, () => {
-    console.log(`Microservice online`);
+  console.log(`Microservice is now online`);
 });
